bda495113a7042f8da996669acb60207b55f309c
sarahdingg <145302047+sarahdingg@users.noreply.github.com>
2024-11-21 16:54:31 -0500

    Update 03-clean_data.R

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,11 +1,16 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Cleans the raw data obtained from Spotify API
+# Author: 
+  - Sarah Ding
+  - Jamie Lee
+  - Vandan Patel
+  - Prankit Bhardwaj
+  - Dong Jun Yoon
+# Date: 21 November 2024
+# Contact: sarah.ding@mail.utoronto.ca
+# License: UofT
+# Pre-requisites: make sure raw data is saved
+# Any other information needed? N/A
 
 #### Workspace setup ####
 library(tidyverse)
